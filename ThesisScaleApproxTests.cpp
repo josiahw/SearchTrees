9da91fbe1e927110f3fcc060262c8027263025d8
Josiah Walker <josiah.walker@uon.edu.au>
2017-10-09 09:02:14 +1100

    refactor scale tests to run a bit faster

--- ThesisScaleApproxTests.cpp
+++ ThesisScaleApproxTests.cpp
@@ -28,53 +28,97 @@ int main (void) {
     std::pair<double, double> times;
     double accuracy = 0.;
     double stddev = 0.;
-    
+
     std::vector<double> s1Vals, const1Vals;
-    for (size_t j = 0; j <= 100; ++j) {
+    std::vector<size_t> leafVals;
+    std::vector<std::string> scaletype;
+    for (size_t j = 0; j < 60; ++j) {
         double i = j/100.;
         s1Vals.push_back(i);
         const1Vals.push_back(1.0);
+        leafVals.push_back(std::numeric_limits<size_t>::max());
+        scaletype.push_back("tri-gen");
     }
-    
+    for (size_t j = 6; j <= 10; ++j) {
+        double i = j/10.;
+        s1Vals.push_back(i);
+        const1Vals.push_back(1.0);
+        leafVals.push_back(std::numeric_limits<size_t>::max());
+        scaletype.push_back("s-approx");
+    }
+    for (size_t j = 0; j < 6; ++j) {
+        double i = j/10.;
+        const1Vals.push_back(i);
+        s1Vals.push_back(1.0);
+        leafVals.push_back(std::numeric_limits<size_t>::max());
+        scaletype.push_back("tri-gen");
+    }
+    for (size_t j = 180; j <= 300; ++j) {
+        double i = j/300.;
+        const1Vals.push_back(i);
+        s1Vals.push_back(1.0);
+        leafVals.push_back(std::numeric_limits<size_t>::max());
+        scaletype.push_back("tri-gen");
+    }
+    for (size_t j = 0; j < 200; j += 20) {
+        const1Vals.push_back(1.0);
+        s1Vals.push_back(1.0);
+        leafVals.push_back(j);
+        scaletype.push_back("leaf-limit");
+    }
+    for (size_t j = 200; j < 1000; j += 50) {
+        const1Vals.push_back(1.0);
+        s1Vals.push_back(1.0);
+        leafVals.push_back(j);
+        scaletype.push_back("leaf-limit");
+    }
+    for (size_t j = 1000; j < 5000; j += 100) {
+        const1Vals.push_back(1.0);
+        s1Vals.push_back(1.0);
+        leafVals.push_back(j);
+        scaletype.push_back("leaf-limit");
+    }
+
+    std::cout << "APPROX VALUE, SCALE TYPE, DATASET, ACCURACY, TIME" << std::endl;
+
     using Test = ThesisTest<CoverTree<DataReferenceEuclideanNode, DataReferenceEuclideanNode>>;
     const size_t testSize = 1000000;
-    const size_t testQueries = 10000;
+    const size_t testQueries = 1000;
+
     //load the SIFT dataset
     DataSet sift = Test::mapData("sift_mmapready", 128, 1000000);
     DataSet siftq = Test::mapData("sift_queries_mmapready", 128, 10000);
-    
-    auto sifts1results = Test::statsSVals(10, testSize, testQueries, s1Vals, const1Vals, sift, siftq);
-    auto sifts2results = Test::statsSVals(10, testSize, testQueries, const1Vals, s1Vals, sift, siftq);
-    std::cout << "SIFT build times: " << sifts1results.buildTime << ", " << sifts2results.buildTime << std::endl;
-    
+    auto sifts1results = Test::statsSVals(10, testSize, testQueries, s1Vals, const1Vals, leafVals, sift, siftq);
     Test::unMapData(sift);
     Test::unMapData(siftq);
-    
+
+    for (size_t j = 0; j < s1Vals.size(); ++j) {
+        if (scaletype[j] == "s-approx") {
+            std::cout << s1Vals[j] << ", ";
+        } else if (scaletype[j] == "tri-gen") {
+            std::cout << const1Vals[j] << ", ";
+        } else {
+            std::cout << leafVals[j] << ", ";
+        }
+        std::cout << scaletype[j] << ", SIFT, " << sifts1results.accuracy[j] << " +/- " << sifts1results.stddev[j] << ", " << sifts1results.queryTime[j] << std::endl;
+    }
+
     //load the GIST dataset
     sift = Test::mapData("gist_mmapready", 960, 1000000);
     siftq = Test::mapData("gist_queries_mmapready", 960, 10000);
-    
-    auto gists1results = Test::statsSVals(10, testSize, testQueries, s1Vals, const1Vals, sift, siftq);
-    auto gists2results = Test::statsSVals(10, testSize, testQueries, const1Vals, s1Vals, sift, siftq);
-    std::cout << "GIST build times: " << gists1results.buildTime << ", " << gists2results.buildTime << std::endl;
-    
+    auto gists1results = Test::statsSVals(10, testSize, testQueries, s1Vals, const1Vals, leafVals, sift, siftq);
     Test::unMapData(sift);
     Test::unMapData(siftq);
-    
-    std::cout << "S VALUE, SCALE TYPE, SIFT TIME, SIFT ACCURACY, GIST TIME, GIST ACCURACY" << std::endl;
+
     for (size_t j = 0; j < s1Vals.size(); ++j) {
-        std::cout << s1Vals[j] << ", S-APPROX, " << 
-                    sifts1results.queryTime[j] << ", " << 
-                    sifts1results.accuracy[j] << " +/- " << sifts1results.stddev[j] << ", " <<
-                    gists1results.queryTime[j] << ", " << 
-                    gists1results.accuracy[j] << " +/- " << gists1results.stddev[j] << ", " <<
-                    std::endl;
-        std::cout << s1Vals[j] << ", TRI-GEN, " << 
-                    sifts2results.queryTime[j] << ", " << 
-                    sifts2results.accuracy[j] << " +/- " << sifts2results.stddev[j] << ", " <<
-                    gists2results.queryTime[j] << ", " << 
-                    gists2results.accuracy[j] << " +/- " << gists2results.stddev[j] << ", " <<
-                    std::endl;
+        if (scaletype[j] == "s-approx") {
+            std::cout << s1Vals[j] << ", ";
+        } else if (scaletype[j] == "tri-gen") {
+            std::cout << const1Vals[j] << ", ";
+        } else {
+            std::cout << leafVals[j] << ", ";
+        }
+        std::cout << scaletype[j] << ", GIST, " << gists1results.accuracy[j] << " +/- " << gists1results.stddev[j] << ", " << gists1results.queryTime[j] << std::endl;
     }
 
     return 0;
